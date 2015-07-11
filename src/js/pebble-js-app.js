63bc6ccbdf916110018a2ed92d3c4f01bcf1522d
C-D-Lewis <Bonsitm@gmail.com>
2015-07-10 18:35:10 -0700

    Pass config data to C app

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -3,11 +3,27 @@ Pebble.addEventListener('ready', function() {
 });
 
 Pebble.addEventListener('showConfiguration', function() {
-  var url = 
+  var url = 'https://rawgit.com/pebble-examples/design-guides-config-example/dev/config/index.html';
   console.log('Showing configuration page: ' + url);
+
+  Pebble.openURL(url);
 });
 
-Pebble.addEventListener('webviewclosed', function(dict) {
-  console.log('Received config data: ' + JSON.stringify(dict.payload));
+Pebble.addEventListener('webviewclosed', function(e) {
+  var configData = JSON.parse(decodeURIComponent(e.response));
+  console.log('Configuration page returned: ' + JSON.stringify(configData));
 
+  var backgroundColor = configData['background_color'];
+
+  // Send to watchapp
+  Pebble.sendAppMessage({
+    'KEY_COLOR_RED': parseInt(backgroundColor.substring(2, 4), 16),
+    'KEY_COLOR_GREEN': parseInt(backgroundColor.substring(4, 6), 16),
+    'KEY_COLOR_BLUE': parseInt(backgroundColor.substring(6), 16),
+    'KEY_HIGH_CONTRAST': configData['high_contrast']
+  }, function() {
+    console.log('Send successful!');
+  }, function() {
+    console.log('Send failed!');
+  });
 });
--- src/main.c
+++ src/main.c
@@ -1,7 +1,31 @@
 #include <pebble.h>
 
+#define KEY_COLOR_RED     0
+#define KEY_COLOR_GREEN   1
+#define KEY_COLOR_BLUE    2
+#define KEY_HIGH_CONTRAST 3
+
 static Window *s_main_window;
 static TextLayer *s_text_layer;
+
+static void inbox_received_handler(DictionaryIterator *iter, void *context) {
+  // Animated?
+  Tuple *high_contrast_t = dict_find(iter, KEY_HIGH_CONTRAST);
+  if(high_contrast_t && t->value->int32 > 0) {
+    // Change color scheme
+    window_set_background_color(s_main_window, GColorWhite);
+    text_layer_set_text_color(s_text_layer, GColorBlack);
+
+    // Persist value
+    persist_write_bool(KEY_HIGH_CONTRAST, true);
+  }
+
+  // Color scheme?
+  Tuple *color_red_t = dict_find(iter, KEY_COLOR_RED);
+  if(color_red_t) {
+    
+  }
+}
 
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
@@ -25,6 +49,9 @@ static void init() {
     .unload = window_unload,
   });
   window_stack_push(s_main_window, true);
+
+  app_message_register_inbox_received(inbox_received_handler);
+  app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
 
 static void deinit() {
